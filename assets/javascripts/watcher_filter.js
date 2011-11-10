00959c2716ef3b583cde39de0b785515427bf39d
ayweak <ayweak@users.noreply.github.com>
2011-11-11 03:05:09 +0900

    ユーザ名でフィルタする機能を追加。ただし検証途中

--- assets/javascripts/watcher_filter.js
+++ assets/javascripts/watcher_filter.js
@@ -1,21 +1,32 @@
 (function() {
+    var defaultNameValue = "";
+    var noNameInput = true;
+
     function init() {
         var groupSelectId = "watcher_filter_group_select";
         var roleSelectId = "watcher_filter_role_select";
+        var nameInputId = "watcher_filter_name_input";
+        var applyButtonId = "watcher_filter_apply_button";
+        var clearButtonId = "watcher_filter_clear_button";
         var checkButtonId = "watcher_filter_check_button";
         var uncheckButtonId = "watcher_filter_uncheck_button";
         var checkboxName = "issue[watcher_user_ids][]";
 
         var groupSelect = document.getElementById(groupSelectId);
         var roleSelect = document.getElementById(roleSelectId);
+        var nameInput = document.getElementById(nameInputId);
+        var applyButton = document.getElementById(applyButtonId);
+        var clearButton = document.getElementById(clearButtonId);
         var checkButton = document.getElementById(checkButtonId);
         var uncheckButton = document.getElementById(uncheckButtonId);
+
+        defaultNameValue = nameInput.value;
 
         addEventListener(
             groupSelect,
             "change",
             function() {
-                filterWatchers(groupSelectId, roleSelectId, checkboxName);
+                filterWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
             }
         );
 
@@ -23,7 +34,36 @@
             roleSelect,
             "change",
             function() {
-                filterWatchers(groupSelectId, roleSelectId, checkboxName);
+                filterWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
+            }
+        );
+
+        addEventListener(
+            nameInput,
+            "focus",
+            function() { setBlankName(nameInputId); }
+        );
+
+        addEventListener(
+            nameInput,
+            "blur",
+            function() { setDefaultName(nameInputId); }
+        );
+
+        addEventListener(
+            applyButton,
+            "click",
+            function() {
+                filterWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
+            }
+        );
+
+        addEventListener(
+            clearButton,
+            "click",
+            function() {
+                clearConditions(groupSelectId, roleSelectId, nameInputId);
+                filterWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
             }
         );
 
@@ -31,7 +71,7 @@
             checkButton,
             "click",
             function() {
-                checkWatchers(groupSelectId, roleSelectId, checkboxName);
+                checkWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
             }
         );
 
@@ -39,7 +79,7 @@
             uncheckButton,
             "click",
             function() {
-                uncheckWatchers(groupSelectId, roleSelectId, checkboxName);
+                uncheckWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName);
             }
         );
     }
@@ -52,46 +92,80 @@
         }
     }
 
-    function filterWatchers(groupSelectId, roleSelectId, checkboxName) {
+    function setBlankName(nameInputId) {
+        var nameInput = document.getElementById(nameInputId);
+
+        if (noNameInput) {
+            nameInput.value = "";
+        }
+    }
+
+    function setDefaultName(nameInputId) {
+        var nameInput = document.getElementById(nameInputId);
+        var value = nameInput.value.replace(/^\s+|\s+$/g, "");
+
+        if (value === "") {
+            nameInput.value = defaultNameValue;
+            noNameInput = true;
+        } else {
+            nameInput.value = value;
+            noNameInput = false;
+        }
+    }
+
+    function clearConditions(groupSelectId, roleSelectId, nameInputId) {
+        var groupSelect = document.getElementById(groupSelectId);
+        var roleSelect = document.getElementById(roleSelectId);
+        var nameInput = document.getElementById(nameInputId);
+
+        groupSelect.selectedIndex = 0;
+        roleSelect.selectedIndex = 0;
+        nameInput.value = defaultNameValue;
+        noNameInput = true;
+    }
+
+    function filterWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName) {
         var func = function(checkbox, condition) {
             checkbox.parentNode.style.display = condition ? "inline" : "none";
         };
 
-        updateWatchers(groupSelectId, roleSelectId, checkboxName, func);
+        updateWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName, func);
     }
 
-    function checkWatchers(groupSelectId, roleSelectId, checkboxName) {
+    function checkWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName) {
         var func = function(checkbox, condition) {
             if (condition) {
                 checkbox.checked = true;
             }
         };
 
-        updateWatchers(groupSelectId, roleSelectId, checkboxName, func);
+        updateWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName, func);
     }
 
-    function uncheckWatchers(groupSelectId, roleSelectId, checkboxName) {
+    function uncheckWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName) {
         var func = function(checkbox, condition) {
             if (condition) {
                 checkbox.checked = false;
             }
         };
 
-        updateWatchers(groupSelectId, roleSelectId, checkboxName, func);
+        updateWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName, func);
     }
 
-    function updateWatchers(groupSelectId, roleSelectId, checkboxName, func) {
-        var groupSelect = document.getElementById(groupSelectId);
-        var roleSelect = document.getElementById(roleSelectId);
-        var userIds = getUserIds(groupSelect, roleSelect);
+    function updateWatchers(groupSelectId, roleSelectId, nameInputId, checkboxName, func) {
+        var userIds = getUserIds(groupSelectId, roleSelectId);
+        var name = noNameInput
+                 ? ""
+                 : document.getElementById(nameInputId).value.toLowerCase();
         var checkboxes = document.getElementsByName(checkboxName);
 
         for (var i = 0; i < checkboxes.length; i++) {
             var userId = checkboxes[i].value;
+            var userName = checkboxes[i].parentNode.lastChild.nodeValue.toLowerCase();
             var j;
 
             for (j = 0; j < userIds.length; j++) {
-                if (userIds[j] === userId) {
+                if (userIds[j] === userId && userName.indexOf(name) >= 0) {
                     break;
                 }
             }
@@ -100,7 +174,9 @@
         }
     }
 
-    function getUserIds(groupSelect, roleSelect) {
+    function getUserIds(groupSelectId, roleSelectId) {
+        var groupSelect = document.getElementById(groupSelectId);
+        var roleSelect = document.getElementById(roleSelectId);
         var groupValues = groupSelect.options[groupSelect.selectedIndex].value.split(",");
         var roleValues = roleSelect.options[roleSelect.selectedIndex].value.split(",");
         var userIds = [];
